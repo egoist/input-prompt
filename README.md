3a6505b4f15341f4f3ab8d067ed5b4a7af599b89
EGOIST <0x142857@gmail.com>
2016-07-27 22:39:42 +0800

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# input-prompt [![NPM version](https://img.shields.io/npm/v/input-prompt.svg)](https://npmjs.com/package/input-prompt) [![NPM downloads](https://img.shields.io/npm/dm/input-prompt.svg)](https://npmjs.com/package/input-prompt) [![Build Status](https://img.shields.io/circleci/project/egoist/input-prompt/master.svg)](https://circleci.com/gh/egoist/input-prompt)
+
+> Input prompt with auto-completion, an ultra simple alternative to Typeahead.js
+
+## Install
+
+```bash
+$ npm install --save input-prompt
+```
+
+`input-prompt` does not do anything by default, but you can write rules to achieve something interesting, see usage below.
+
+## Usage
+
+```js
+const InputPrompt = require('input-prompt')
+
+// which input element to control?
+const prompt = new InputPrompt(document.querySelector('input'))
+
+// I want to auto-complete gmail address when user is typing...
+prompt.addRule({
+  // execute this rule when input value passed the regular expression
+  test: /[^@]+@^$]+/,
+  handler(input) {
+    // how you would like to update the prompt text
+    // here we simply make it a full @gmail.com address
+    const hint = input.match(/([^@]+@)([^$]+)/)
+    if (hint[2][0] === 'g') {
+      return `${hint[1]}gmail.com`
+    }
+    return input
+  }
+})
+
+// initial the element
+prompt.init()
+// cheers! 🍻
+```
+
+When the prompt shows you can press <kbd>→</kbd> or <kbd>tab</kbd> to apply it to the real `input` element.
+
+A live example: https://egoistian.com/input-prompt
+
+## License
+
+MIT © [EGOIST](https://github.com/egoist)
---
+++ src/input-prompt.js
@@ -0,0 +1,71 @@
+const keys = {
+  right: 39,
+  tab: 9
+}
+
+function css(el, style) {
+  for (const key in style) {
+    el.style[key] = style[key]
+  }
+}
+
+export default class InputPrompt {
+  constructor(el) {
+    this.el = el
+    this.rules = []
+  }
+
+  addRule({test, handler}) {
+    this.rules.push({
+      test,
+      handler
+    })
+  }
+
+  init() {
+    this.wrapper = document.createElement('div')
+    this.wrapper.className = 'input-prompt'
+    this.wrapper.style.position = 'relative'
+
+    this.el.parentNode.insertBefore(this.wrapper, this.el)
+    this.el.parentNode.removeChild(this.el)
+    this.wrapper.appendChild(this.el)
+    css(this.el, {
+      position: 'absolute',
+      zIndex: 1,
+      backgroundColor: 'transparent'
+    })
+
+    this.fakeEl = document.createElement(this.el.tagName)
+    this.fakeEl.className = this.el.className
+    css(this.fakeEl, {
+      position: 'absolute',
+      left: `${this.el.offsetLeft}px`,
+      zIndex: 0,
+      color: '#999'
+    })
+    this.wrapper.appendChild(this.fakeEl)
+
+    this.el.addEventListener('input', e => {
+      this.currentValue = e.target.value
+      this.applyRule()
+    }, false)
+
+    this.el.addEventListener('keydown', e => {
+      if (e.which === keys.right || e.which === keys.tab) {
+        e.preventDefault()
+        this.el.value = this.fakeEl.value
+      }
+    }, false)
+  }
+
+  applyRule() {
+    let result = this.currentValue
+    this.rules.forEach(rule => {
+      if (rule.test.test(this.currentValue)) {
+        result = rule.handler(result)
+      }
+    })
+    this.fakeEl.value = result
+  }
+}
