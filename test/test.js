1738464ab7e09511517cccf66d21ee2765dd6bd9
EGOIST <0x142857@gmail.com>
2016-07-28 23:57:13 +0800

    add browser test

---
+++ test/test.js
@@ -0,0 +1,43 @@
+var expect = chai.expect
+var simulate = require('simulate-event').simulate
+
+function createExample() {
+  var example = document.createElement('div')
+  document.body.appendChild(example)
+  return example
+}
+
+function trigger(el, eventName, payload) {
+  const event = new CustomEvent(eventName, {detail: payload})
+  el.dispatchEvent(event)
+}
+
+describe('main', function () {
+  it('insert new fake input element', function () {
+    var example = createExample()
+
+    var input = document.createElement('input')
+    example.appendChild(input)
+
+    var prompt = new InputPrompt(input)
+    prompt.init()
+    var fake = example.querySelector('.fake-input')
+    expect(fake).to.be.ok
+  })
+
+  it('update fake value while typing', function () {
+    var example = createExample()
+
+    var input = document.createElement('input')
+    example.appendChild(input)
+
+    var prompt = new InputPrompt(input)
+    prompt.init()
+
+    // update input value
+    input.value = 'foo'
+    simulate(input, 'input')
+
+    expect(example.querySelector('.fake-input').value).to.equal('foo')
+  })
+})
